5615c9cefbab7adfaeea25f8c4b956149a57a4fa
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2024-06-28 22:08:42 +0300

    Wow it was a mission to setup Rust for the first time = Developer afterall or ?

---
+++ README.md
@@ -0,0 +1,6 @@
+### I have no words but here we are Rust memory tracker => FUN
+
+## Practice app for study purpouses 
+
+
+# Author Maksim Verzbitski 
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+﻿use ferris_says::say; // Just following the guide
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    // more knoweledge here
+    let stdout = stdout();
+
+    let sms = String::from("Ебало заверни свое пыль, пахта, сметана, закончили!!!!");
+
+    let width = sms.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+
+    say(&sms, width, &mut writer).unwrap();
+
+    //println!("Hello, world!");
+}
